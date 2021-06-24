db555735b03aaf0eb025972a16a042270a231c9e
Yeom <pwlsghq@naver.com>
2021-06-25 00:03:28 +0900

    20210625

--- 1260.py
+++ 1260.py
@@ -1,14 +1,39 @@
+from collections import deque
+from typing import Deque
+
+
+def dfs(root, visited):
+    visited[root] = True
+    print(root, end=' ')
+    for i in range(n+1):
+        if visited[i] == False and graph[root][i] == 1:
+            dfs(i, visited)
+
+
+def bfs(root, visited):
+    visited[root] = True
+    que = deque([root])
+    while que:
+        v = que.popleft()
+        print(v, end=' ')
+        for i in range(n+1):
+            if visited[i] == False and graph[v][i] == 1:
+                que.append(i)
+                visited[i] = True
+
+
 n, m, v = map(int, input().split())
-print(n, m, v)
-nodes = []
 
-for n in range(m):
-    nodes.append(list(map(int, input().split())))
-print(nodes)
-graph = [[] for _ in range(n+1)]
-print(graph)
-for node in nodes:
-    graph[node[0]].append(node[1])
-    graph[node[1]].append(node[0])
+graph = [[0]*(n+1) for _ in range(n+1)]
 
-print(graph)
+for i in range(m):
+    x, y = map(int, input().split())
+    graph[x][y] = 1
+    graph[y][x] = 1
+
+visited = [False]*(n+1)
+dfs(v, visited)
+print()
+visited = [False]*(n+1)
+bfs(v, visited)
+print()
