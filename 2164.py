385e848f8bfcf779eaa3746cc17ab2e04b99d92c
Yeom <pwlsghq@naver.com>
2021-07-06 18:41:37 +0900

    20210706

---
+++ 2164.py
@@ -0,0 +1,12 @@
+import sys
+from typing import Deque
+
+q = Deque()
+for i in range(int(sys.stdin.readline())):
+    q.append(i+1)
+
+while len(q) > 1:
+    print(q)
+    q.popleft()
+    q.append(q.popleft())
+print(q[0])
