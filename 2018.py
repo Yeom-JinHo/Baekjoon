adeaffaa22b9819c6f872b6a81fddc49e30dff2b
Yeom <pwlsghq@naver.com>
2021-07-10 12:12:34 +0900

    20210710

--- 2018.py
+++ 2018.py
@@ -1,1 +1,28 @@
-n = int(input())
+import sys
+import math
+n = int(sys.stdin.readline())
+ls = []
+dls = []
+dic = dict()
+for i in range(0, n):
+    inp = int(sys.stdin.readline())
+    ls.append(inp)
+    if(inp in dic):
+        dic[inp] += 1
+    else:
+        dic[inp] = 1
+dic = sorted(dic.items(), key=lambda item: item[1], reverse=True)
+
+for i in range(len(dic)):
+    if(dic[i][1] == dic[0][1]):
+        dls.append(dic[i][0])
+ls.sort()
+dls.sort()
+print(round(sum(ls)/n))
+print(ls[math.floor(n/2)])
+
+if(len(dls) == 1):
+    print(dls[0])
+else:
+    print(dls[1])
+print(max(ls)-min(ls))
