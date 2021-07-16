e0c712f4499b80c1338959b19313c475aa0c8050
Yeom <pwlsghq@naver.com>
2021-07-16 13:42:15 +0900

    20210716

--- 1065.py
+++ 1065.py
@@ -1,8 +1,8 @@
-n=int(input())
-ls=[]
-for i in range(1,n+1):
-    if(i//100!=0):
-        if(i//100-i//10%10==i//10%10-i%10):
+n = int(input())
+ls = []
+for i in range(1, n+1):
+    if(i//100 != 0):
+        if(i//100-i//10 % 10 == i//10 % 10-i % 10):
             ls.append(int(i))
     else:
         ls.append(i)
