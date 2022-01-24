9dcd0e93077ccf57195aab29f44872025e924f83
Yeom-JinHo <pwlsghq@naver.com>
2022-01-24 21:04:12 +0900

    20220124

---
+++ R_1018.py
@@ -0,0 +1,33 @@
+import sys
+
+def check(r,c):
+  bCount=0;
+  wCount=0;
+  for i in range(8):
+    for j in range(8):
+      if board[i+r][j+c]==wBoard[i][j]:
+        wCount+=1;
+      if board[i+r][j+c]==bBoard[i][j]:
+        bCount+=1;
+  return wCount if wCount<bCount else bCount;
+
+
+wBoard=['WBWBWBWB','BWBWBWBW',
+        'WBWBWBWB','BWBWBWBW',
+        'WBWBWBWB','BWBWBWBW',
+        'WBWBWBWB','BWBWBWBW'];
+bBoard=['BWBWBWBW','WBWBWBWB',
+        'BWBWBWBW','WBWBWBWB',
+        'BWBWBWBW','WBWBWBWB',
+        'BWBWBWBW','WBWBWBWB'];
+
+n,m=map(int,sys.stdin.readline().split());
+
+board=list();
+for r in range(n):
+  board.append(sys.stdin.readline().rstrip());
+result=list();
+for r in range(n-7):
+  for c in range(m-7):
+    result.append(check(r,c));
+print(min(result));
