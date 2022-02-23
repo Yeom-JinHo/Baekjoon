15c8edda0d5c22e384008c527693ba2349afc441
Yeom-JinHo <pwlsghq@naver.com>
2022-02-23 14:08:46 +0900

    20220223

---
+++ 16724.py
@@ -0,0 +1,54 @@
+from gettext import find
+import sys
+def dfs(sr,sc):  
+  i=dd.index(arr[sr][sc]);
+  tr=sr+dr[i];
+  tc=sc+dc[i];
+  union(sr,sc,tr,tc)
+
+def union(sr,sc,er,ec):
+  aRoot =findParents(sr,sc);
+  bRoot =findParents(er,ec);
+  if aRoot!=bRoot:
+    parents[sr][sc]=parents[er][ec];
+
+def findParents(r,c):
+  if r*C+c==parents[r][c]:
+    return r*C+c
+  parents[r][c]=findParents(parents[r][c]//C,parents[r][c]%C);
+  return parents[r][c];
+
+# u d l r
+dr=[-1,1,0,0];
+dc=[0,0,-1,1];
+dd=['U','D','L','R'];
+R,C=map(int,sys.stdin.readline().split());
+arr=list();
+
+parents=[[0]*C for _ in range(R)];
+count=0;
+for r in range(R):
+  for c in range(C):
+    parents[r][c]=count;
+    count+=1;
+
+for _ in range(R):
+  arr.append(list(sys.stdin.readline().rstrip()))
+
+for r in range(R):
+  for c in range(C):
+      dfs(r,c);
+
+chk=[False]*(R*C);
+
+for r in range(R):
+  for c in range(C):
+    parents[r][c]=findParents(r,c);
+answer=0;
+for r in range(R):
+  for c in range(C):
+    if not chk[parents[r][c]]:
+      chk[parents[r][c]]=True;
+      answer+=1;
+
+print(answer);
---
+++ Main_2527_염진호.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main_2527_염진호{
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+    for(int i=0; i<4;i++){
+      st= new StringTokenizer(br.readLine());
+      int min_X_1=Integer.parseInt(st.nextToken());
+      int min_Y_1=Integer.parseInt(st.nextToken());
+      int max_X_1=Integer.parseInt(st.nextToken());
+      int max_Y_1=Integer.parseInt(st.nextToken());
+
+      int min_X_2=Integer.parseInt(st.nextToken());
+      int min_Y_2=Integer.parseInt(st.nextToken());
+      int max_X_2=Integer.parseInt(st.nextToken());
+      int max_Y_2=Integer.parseInt(st.nextToken());
+      // 9구역으로 나눠보자
+      // 1 |2| 3
+      // 4 |X| 5
+      // 6 |7| 8
+      // 1 4 6 처리
+      if(min_X_1>=max_X_2){
+        // 선분 or 점
+        if(min_X_1==max_X_2){
+          // 1 처리
+          if(max_Y_1<=min_Y_2){
+            if(max_Y_1==min_Y_2){
+              bw.write("c"+"\n");
+              continue;
+            }
+            bw.write("d"+"\n");
+            continue;
+          }
+
+          // 6 처리
+          if(min_Y_1>=max_Y_2){
+            if(min_Y_1==max_Y_2){
+              bw.write("c"+"\n");
+              continue;
+            }
+            bw.write("d"+"\n");
+            continue;
+          }
+          // 자동으로 4처리 가능
+          bw.write("b"+"\n");
+          continue;
+        }
+        bw.write("d"+"\n");
+        continue;
+      }
+      // 9구역으로 나눠보자
+      // 1 |2| 3
+      // 4 |X| 5
+      // 6 |7| 8
+
+      // 3 5 8 처리
+      if(max_X_1<=min_X_2){
+
+        if(max_X_1==min_X_2){
+          //3 처리
+          if(max_Y_1<=min_Y_2){
+            if(max_Y_1==min_Y_2){
+              bw.write("c"+"\n");
+              continue;
+            }
+            bw.write("d"+"\n");
+            continue;
+          }
+          //8 처리
+          if(min_Y_1>=max_Y_2){
+            if(min_Y_1==max_Y_2){
+              bw.write("c"+"\n");
+              continue;
+            }
+            bw.write("d"+"\n");
+            continue;
+          }
+          // 자동으로 5처리
+          bw.write("b"+"\n");
+          continue;
+        }
+        bw.write("d"+"\n");
+        continue;
+      }
+      // 9구역으로 나눠보자
+      // 1 |2| 3
+      // 4 |X| 5
+      // 6 |7| 8
+
+      // 2 처리
+      if (max_Y_1<=min_Y_2){        
+        if(max_Y_1==min_Y_2){
+          bw.write("b"+"\n");
+          continue;
+        }
+        bw.write("d"+"\n");
+        continue;
+      }
+
+      // 7 처리
+      if (min_Y_1>=max_Y_2){
+        if(min_Y_1==max_Y_2){
+          bw.write("b"+"\n");
+          continue;
+        }
+        bw.write("d"+"\n");
+        continue;
+      }
+
+      bw.write("a"+"\n");
+      continue;
+    }
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
